e0a558fd4afe06af30af3e8f2201e2082c83b7cd
Felipe Marinho <felipevm97@gmail.com>
2017-12-08 19:29:29 -0300

    CDE 2.6.1 and bootstrap

--- works/consulta-up.js
+++ works/consulta-up.js
@@ -1,3 +1,8 @@
+$(window).on('resize', () => {
+  if ($(window).width() <= 640) {
+    $('.downloadLink').html("Download");
+  }
+});
 $(function() {
   console.log($(window).width());
   if ($(window).width() <= 640) {
