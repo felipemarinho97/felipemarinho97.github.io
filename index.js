3da258af3bd34060b8381df07ceafcb298534b83
Felipe Marinho <felipevm97@gmail.com>
2018-03-25 22:20:18 -0300

    removendo bad-smells + ES6

--- index.js
+++ index.js
@@ -1,160 +1,143 @@
 $(function() {
-  var sectionHeader = $('.sectionHeader');
+  let sectionHeader = $('.sectionHeader');
+  let picture = $('.picture');
+  let description = $('.description');
+  let descriptionH3 = $('.description > h3');
+  let social = $('.social');
+
   $(window).scroll(function() {
-    if ($(this).scrollTop() > 264 - 50) {
-      $('.sectionHeader').css({
+    let larguraDaTela = $(this).width();
+    let scrollTop = $(this).scrollTop();
+
+    if (scrollTop > 264 - 50) {
+      sectionHeader.css({
         'position': 'fixed',
         'max-height': '50px',
         'text-align': 'center',
         'float': 'none'
       })
-      $('.picture').css({
+
+      picture.css({
         'width': '52.5px',
         'height': '52.5px',
         'position': 'relative',
         'right': '41%'
       })
-      $('.description').css({
+      
+      description.css({
         'position': 'fixed',
         'top': '-1.71px',
         'left': '0%',
-        'right': '0%'
-      })
-      $('.description').css({
+        'right': '0%',
         'opacity': '1'
       })
-      $('.description > h3').css({
+      
+      descriptionH3.css({
         'opacity': '0'
       })
 
-      if ($(this).width() > 640) {
-        $('.social').css({
+      // Aqui estão os casos com medidas pré-definidas para 
+      // diferentes tamanhos de telas.
+      if (larguraDaTela > 640) {
+        social.css({
           'left': '41%',
           'position': 'relative',
           'top': '-40.04px',
           'right': '0%'
         })
-      } else if ($(this).width() > 500) {
-          $('.social').css({
+      } else if (larguraDaTela > 500) {
+          social.css({
             'left': '36.5%',
             'position': 'relative',
             'top': '-40.04px',
             'right': '0%'
           })
-      } else if ($(this).width() > 240) {
-        $('.social').css({
+      } else if (larguraDaTela > 240) {
+        social.css({
           'left': '29.5%',
           'position': 'relative',
           'top': '-40.04px',
           'right': '0%'
         })
-        $('.description').css({
+        description.css({
           'opacity': '0'
         })
       } else {
-        $('.social').css({
+        social.css({
           'left': '18%',
           'position': 'relative',
           'top': '-40.04px',
           'right': '0%'
         })
-        $('.description').css({
+        description.css({
           'opacity': '0'
         })
       }
-      //console.log(sectionHeader.css('height'));
-      // $('section').css({
-      //   'padding-top': sectionHeader.css('height')
-      // })
-      // $('.picture').css({
-      //   'width': '50px',
-      //   'height': '50px'
-      // })
-      // $('.social').css({
-      //   'position': 'absolute',
-      //   'top': '10px',
-      //   'right': '10px',
-      // })
-      // $('.description > h3').hide()
-      // $('.description').css({
-      //   'position': 'absolute',
-      //   'top': '0',
-      //   'left': '150px'
-      // });
-      // sectionHeader.css({
-      //   'position': 'fixed',
-      //   'top': '0px',
-      //   'left': '0',
-      //   'right': '0',
-      //   'height': '60px',
-      //   'text-align': 'left',
-      //   'float': 'right'
-      // });
     } else {
-      var percentOfSectionScrolled = parseInt(((264 - $(this).scrollTop())*100)/264);
-      var lateralDistance = 50 - (percentOfSectionScrolled/2);
-      // console.log(100-percentOfSectionScrolled);
+      let percentOfSectionScrolled = parseInt(((264 - scrollTop)*100)/264);
+      let lateralDistance = 50 - (percentOfSectionScrolled/2);
+
       if (lateralDistance < 50) {
-        $('.picture').css({
+        picture.css({
           'position': 'relative',
           'right': lateralDistance + '%'
         })
-        if ($(this).width() > 640) {
-          $('.social').css({
+
+        if (larguraDaTela > 640) {
+          social.css({
             'left': lateralDistance + '%'
           })
-        } else if ($(this).width() > 500) {
+        } else if (larguraDaTela > 500) {
           if (lateralDistance < 37) {
-            $('.social').css({
+            social.css({
               'left': lateralDistance + '%'
             })
           }
         } else {
           if (lateralDistance < 30) {
-            $('.social').css({
+            social.css({
               'left': lateralDistance + '%'
             })
-            $('.description').css({
+            description.css({
               'opacity': 1-((lateralDistance/100)*4)
             })
           }
         }
-//console.log((125*((100-percentOfSectionScrolled)/100)))
-        // $('.description').css({
-        //   'position': 'relative',
-        //   // 'top': (125*((100-percentOfSectionScrolled)/100)) + 'px',
-        //
-        // });
       }
+      
       if (lateralDistance < 30) {
-        $('.picture').css({
+        picture.css({
           'width': 125 - (125*((100-percentOfSectionScrolled)/100)) + 'px',
           'height': 125 - (125*((100-percentOfSectionScrolled)/100)) + 'px'
         })
       }
+
       $('section').css({
-        'padding-top': (264 - $(this).scrollTop()/100) + 'px'
+        'padding-top': (264 - scrollTop/100) + 'px'
       })
-      $('.social').css({
+      
+      social.css({
         'position': 'relative',
         'top': 182 - (182*((140-percentOfSectionScrolled)/100))+'px',
         'right': '0%'
       })
-      // console.log(145 - (145*((100-percentOfSectionScrolled)/100))+"px")
+      
       if ((100-percentOfSectionScrolled) < 200) {
-        $('.description').css({
+        description.css({
           'position': 'fixed',
           'top': 171 - (171*((119-percentOfSectionScrolled)/100))+'px',
           'left': '0%',
           'right': '0%'
         })
       }
-      $('.description > h3').css({
+      
+      descriptionH3.css({
         'opacity': 1-((lateralDistance/100)*3)
       })
+      
       sectionHeader.css({
         'position': 'fixed',
-        'max-height': (264 - $(this).scrollTop()) + 'px',
+        'max-height': (264 - scrollTop) + 'px',
         'text-align': 'center',
         'float': 'none'
       });
